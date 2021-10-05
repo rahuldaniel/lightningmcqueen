1732a08b2aa16fc337ff73dfc01e3b3cdec18c15
rahulpenumala <56040633+rahulpenumala95@users.noreply.github.com>
2021-10-05 20:14:40 +0530

    Update collatz.py
    Nexchange

--- collatz.py
+++ collatz.py
@@ -1,1 +1,20 @@
-n = int(input("give any number")
+import sys
+#3import matplotlib.pyplot as plt
+n=int(input("any whole number"))
+#x = []
+#y = []
+#c=0
+while True:
+	if n==1:
+#		plt.scatter(x, y)
+#		plt.show()
+		sys.exit()		
+	elif (n%2)==0:
+		n = n/2
+		print(n)
+	elif (n%2) ==1:
+		n = 3*n+1
+		print(n)
+#	x.append(n)
+#	c=c+1
+#	y.append(c)
